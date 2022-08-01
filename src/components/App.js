a89c052828ca02f860b2d43ff929a9a0120793c7
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2022-08-02 01:56:08 +0530

    Final and first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import { render } from "enzyme";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState();
+  const changePara = () => {
+    return setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      {para && <p id="para">{para}</p>} 
+      <button id="click" onClick={changePara}>
+        clickMe
+      </button>
     </div>
   );
 }
-
 
 export default App;
